9dec30588d06e93dd2010916ed5116c4b19775bf
Nico Scordialo <nicoscordialo@googlemail.com>
2019-06-10 12:49:11 +0200

    Mock bindings data and set up var inspector.

--- varInspector/var_list_poly.js
+++ varInspector/var_list_poly.js
@@ -1,24 +1,54 @@
 function var_dic_list() {
 
-    let varlist = [];
-    // console.log("GLOBAL VARLIST IN EXTENSION", global["varlist"]);
+    let exportVars = [];
 
-    // Polyglot not available in this context
-    // console.log(Polyglot.import)
+    // mock bindings
 
-    try {
-        // avoid regarding the console statement as a variable
-        varlist = global["varlist"].filter(variableName => {return variableName && variableName != "console.log(var_dic_list())"})   
-        //console.log("VARLIST", varlist);
-    }
-    catch(error) {
-        //console.log("LOGGING ERROR");
-        //console.log(error);
-    }
+    let x = "testString"
+    Polyglot.export("x", x);
+    let y = 4;
+    Polyglot.export("y", y);
+    let z = [1, 2, "hello"];
+    Polyglot.export("z", z);
+
+    let a = Polyglot.eval("python", "object()")
+    Polyglot.export("a", a);
+
+    let b = Polyglot.eval("python", "'stringi'")
+    Polyglot.export("b", b);
+
+    let c = Polyglot.eval("R", "list(1, 2, 5)")
+    Polyglot.export("c", c);
     
+    let bindings = []
+    bindings.push("x");
+    bindings.push("y");
+    bindings.push("z");
+    bindings.push("a");
+    bindings.push("b");
+    bindings.push("c");
+
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/isArray
+
+
+    Polyglot.export("bindings", bindings);
+
+    let localBindings = Polyglot.import("bindings");
+
+
+    localBindings.forEach((binding) => {
+        let polyglotImport = Polyglot.import(binding);
+        exportVars.push({
+            "varName": binding,
+            "varSize": polyglotImport.length ? polyglotImport.length : '',
+            "varType": typeof polyglotImport == "object" ? (polyglotImport.length ? "array" : "object") : typeof polyglotImport,
+            "varContent": '' + polyglotImport
+        })
+    })
     
-    // returning test json for now
-    return JSON.stringify([{"varName": "varNico", "varType": "int", "varSize": "28", "varShape": "", "varContent": Polyglot.import('varNico')}])
+    // dummy: [{"varName": "varNico", "varType": "int", "varSize": "28", "varShape": "", "varContent": Polyglot.import('varNico')}]
+    
+    return JSON.stringify(exportVars);
 }
 
 console.log(var_dic_list())
